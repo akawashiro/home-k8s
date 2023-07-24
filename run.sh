33bb1a5f3e99bce3a45c1ea19424f67d01c5fa5f
Akira Kawata <akirakawata1@gmail.com>
2023-07-24 20:27:07 +0900

    Fix CIDR

--- run.sh
+++ run.sh
@@ -15,7 +15,4 @@ fi
 
 rm -rf join-command
 mkdir join-command
-ssh vagrant@192.168.11.71 date
-ssh vagrant@192.168.11.72 date
-ssh vagrant@192.168.11.73 date
 ansible-playbook cluster-construction.yaml -i hosts -v
